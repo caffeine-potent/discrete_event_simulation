bc1c89fac04eabc31c609ea57f72f88a5a2ab43a
caffeine-potent <ottoXwagner@gmail.com>
2018-06-03 20:24:27 -0400

    Add compiled hello world.

---
+++ target/debug/deps/discrete_event_simulation-a9086e10d366d6ca.d
@@ -0,0 +1,5 @@
+/home/conic/learn_rust/lab1/discrete_event_simulation/target/debug/deps/discrete_event_simulation-a9086e10d366d6ca: src/main.rs
+
+/home/conic/learn_rust/lab1/discrete_event_simulation/target/debug/deps/discrete_event_simulation-a9086e10d366d6ca.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/discrete_event_simulation.d
@@ -0,0 +1,1 @@
+/home/conic/learn_rust/lab1/discrete_event_simulation/target/debug/discrete_event_simulation: /home/conic/learn_rust/lab1/discrete_event_simulation/src/main.rs
